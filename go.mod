07d2e33c940e53e8a2524204c41b119eefeb6b62
Alex Deakins <>
2021-04-07 21:54:48 +0100

    feat: setup data structures in models module

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/google/go-github/v34 v34.0.0 // indirect
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
 )
--- models/models.go
+++ models/models.go
@@ -1,1 +1,51 @@
 package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type ApiClient struct {
+	clientId    uuid.UUID
+	secretKey   uuid.UUID
+	appName     string
+	description string
+	validTill   time.Time
+	updated     time.Time
+	userId      uuid.UUID
+}
+
+type User struct {
+	userId            uuid.UUID
+	name              string
+	email             string
+	encryptedPassword string
+	organisation      string
+	created           time.Time
+	updated           time.Time
+	githubUserId      string
+	apiClients        []ApiClient
+}
+
+type RepoItem struct {
+	itemId      uuid.UUID
+	timeId      int
+	itemType    string
+	source      string
+	created     time.Time
+	repooName   string
+	description string
+}
+
+type TimeEntry struct {
+	entryId   int
+	userId    uuid.UUID
+	created   time.Time
+	updated   uuid.UUID
+	comments  string
+	tags      []string
+	repoItems []RepoItem
+	value     float32
+	valueType string
+}
