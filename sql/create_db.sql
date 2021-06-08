f50938f84a6a865a8214a588ab8cdb7fdadc9160
Alex Deakins <>
2021-06-08 22:33:29 +0100

    Fix: fix errors in create_db.sql

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -1,114 +1,139 @@
 --CREATE DATABASE timetracker;
 
-CREATE TABLE User (
-    id UUID PRIMARY KEY NOT NULL
-    name VARCHAR(200) NOT NULL
-    email VARCHAR(400)
-    created TIMESTAMP NOT NULL DEFAULT NOW()
-    updated TIMESTAMP NOT NULL DEFAULT NOW()
-    githubUserId VARCHAR(100)
+SHOW SEARCH_PATH;
+
+SET SEARCH_PATH TO timetracker;
+
+DROP TABLE IF EXISTS public.UserOrgLink;
+DROP TABLE IF EXISTS public.TimeEntryTagLink;
+DROP TABLE IF EXISTS public.Tag;
+DROP TABLE IF EXISTS public.RepoItem;
+DROP TABLE IF EXISTS public.TimeEntry;
+DROP TABLE IF EXISTS public.ApiClient;
+DROP TABLE IF EXISTS public.Organisation;
+DROP TABLE IF EXISTS public.User;
+
+CREATE TABLE public.User(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    email VARCHAR(400),
+    created TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated TIMESTAMP NOT NULL DEFAULT NOW(),
+    githubUserId VARCHAR(100),
     avatar TEXT
 );
 
-CREATE TABLE Organisation (
-    id UUID PRIMARY KEY NOT NULL
-    name VARCHAR(200) NOT NULL
-    decription varchar(500)
-    avatar TEXT
-    source VARCHAR(50)
-    created TIMESTAMP NOT NULL DEFAULT NOW()
+CREATE INDEX IX_user_githubUserId ON public.User (githubuserid);
+CREATE INDEX IX_user_email ON public.User (email);
+
+CREATE TABLE public.Organisation(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    description varchar(500),
+    avatar TEXT,
+    source VARCHAR(50),
+    created TIMESTAMP NOT NULL DEFAULT NOW(),
     updated TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE UserOrgLink (
-    organisationId UUID
-    userId UUID
+CREATE TABLE public.UserOrgLink(
+    organisationId INT,
+    userId INT
 );
 
-ALTER TABLE UserOrgLink 
+ALTER TABLE public.UserOrgLink
     ADD CONSTRAINT FK_UserOrgLink_OrganisationId 
         FOREIGN KEY (organisationId)
-        REFERENCES Organisation (id)
+        REFERENCES public.Organisation (id)
         ON DELETE CASCADE;
 
-ALTER TABLE UserOrgLink 
+ALTER TABLE public.UserOrgLink
     ADD CONSTRAINT FK_UserOrgLink_UserId 
         FOREIGN KEY (userId)
-        REFERENCES User (id)
+        REFERENCES public.User (id)
         ON DELETE CASCADE;
 
-CREATE TABLE ApiClient (
-    clientId UUID PRIMARY KEY NOT NULL
-    userId UUID
-    secretKey UUID
-    appName VARCHAR(50) NOT NULL
-    description VARCHAR(500)
-    validTill TIMESTAMP
-    created TIMESTAMP NOT NULL DEFAULT NOW()
+CREATE TABLE public.ApiClient(
+    id SERIAL PRIMARY KEY,
+    clientId UUID,
+    userId INT,
+    secretKey UUID,
+    appName VARCHAR(50) NOT NULL,
+    description VARCHAR(500),
+    validTill TIMESTAMP,
+    created TIMESTAMP NOT NULL DEFAULT NOW(),
     updated TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-ALTER TABLE ApiClient
+CREATE INDEX IX_apiclient_clientId ON ApiClient (clientId);
+
+ALTER TABLE public.ApiClient
     ADD CONSTRAINT FK_ApiClient_UserId
-    REFERENCES User (id)
+    FOREIGN KEY (userId)
+    REFERENCES public.User (id)
     ON DELETE CASCADE;
 
-CREATE TABLE TimeEntry (
-    id INT PRIMARY KEY NOT NULL IDENTITY(1, 1)
-    comments VARCHAR(200)
-    created TIMESTAMP NOT NULL DEFAULT NOW()
-    updated TIMESTAMP NOT NULL DEFAULT NOW()
-    value NUMERIC NOT NULL
-    valueType varchar(20) NOT NULL
-    userId UUID
-    organisationId UUID
+CREATE TABLE public.TimeEntry(
+    id BIGSERIAL PRIMARY KEY,
+    comments VARCHAR(200),
+    created TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated TIMESTAMP NOT NULL DEFAULT NOW(),
+    value NUMERIC NOT NULL,
+    valueType varchar(20) NOT NULL,
+    userId INT,
+    organisationId INT
 );
 
-ALTER TABLE TimeEntry 
+CREATE INDEX IX_timeentry_created ON public.TimeEntry (created);
+CREATE INDEX IX_timeentry_updated ON public.TimeEntry (updated);
+
+ALTER TABLE public.TimeEntry
     ADD CONSTRAINT FK_TimeEntry_OrganisationId 
         FOREIGN KEY (organisationId)
-        REFERENCES Organisation (id);
+        REFERENCES public.Organisation (id);
 
-ALTER TABLE TimeEntry 
+ALTER TABLE public.TimeEntry
     ADD CONSTRAINT FK_TimeEntry_UserId 
         FOREIGN KEY (userId)
-        REFERENCES User (id);
+        REFERENCES public.User (id);
 
-CREATE TABLE RepoItem (
-    id INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
-    itemIdSource VARCHAR(200) NOT NULL
-    itemType VARCHAR(50) NOT NULL
-    source VARCHAR(50) NOT NULL
-    created TIMESTAMP NOT NULL
-    updated TIMESTAMP NOT NULL DEFAULT NOW()
-    description VARCHAR(2000)
-    timeEntryId INT
+CREATE TABLE public.RepoItem(
+    id BIGSERIAL PRIMARY KEY,
+    itemIdSource VARCHAR(200) NOT NULL,
+    itemType VARCHAR(50) NOT NULL,
+    source VARCHAR(50) NOT NULL,
+    created TIMESTAMP NOT NULL,
+    updated TIMESTAMP NOT NULL DEFAULT NOW(),
+    description VARCHAR(2000),
+    timeEntryId BIGINT
 );
 
-ALTER TABLE RepoItem 
+CREATE INDEX IX_repoitem_source ON RepoItem (source);
+
+ALTER TABLE public.RepoItem
     ADD CONSTRAINT FK_RepoItem_TimeEntryId 
         FOREIGN KEY (timeEntryId)
-        REFERENCES TimeEntry (id)
+        REFERENCES public.TimeEntry (id)
         ON DELETE CASCADE;
 
-CREATE TABLE Tag (
-    id INT PRIMARY KEY NOT NULL IDENTITY(1, 1)
+CREATE TABLE public.Tag(
+    id SERIAL PRIMARY KEY,
     name varchar(50)
 );
 
-CREATE TimeEntryTagLink (
-    tagId INT
-    timeEntryId INT
+CREATE TABLE public.TimeEntryTagLink(
+    tagId INT,
+    timeEntryId BIGINT
 );
 
-ALTER TABLE TimeEntryTagLink 
+ALTER TABLE public.TimeEntryTagLink
     ADD CONSTRAINT FK_TimeEntryTagLink_TagId 
         FOREIGN KEY (tagId)
-        REFERENCES Tag (id)
+        REFERENCES public.Tag (id)
         ON DELETE CASCADE;
 
-ALTER TABLE TimeEntryTagLink 
+ALTER TABLE public.TimeEntryTagLink
     ADD CONSTRAINT FK_TimeEntryTagLink_TimeEntryId 
         FOREIGN KEY (timeEntryId)
-        REFERENCES TimeEntry (id)
+        REFERENCES public.TimeEntry (id)
         ON DELETE CASCADE;
