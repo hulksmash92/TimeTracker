3c1e6914d3a61f16460b421c9a9109f257aacb99
Alex Deakins <>
2021-09-02 22:30:09 +0100

    feat: add dummy api endpoint for updating user that always returns true currently

--- routes/routes-user.go
+++ routes/routes-user.go
@@ -6,8 +6,21 @@ import (
 	"timetracker/db"
 	"timetracker/github"
 	"timetracker/helpers"
+	"timetracker/models"
 )
 
+// Handles the user api calls by calling the method used to handle
+// the appropriate HTTP Verb
+func userRouteHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getUser(w, r)
+	case http.MethodPatch:
+		updateUser(w, r)
+	}
+}
+
+// Handles the get user api calls
 func getUser(w http.ResponseWriter, r *http.Request) {
 	token, err := parseTokenFromCookie(r)
 	helpers.HandleError(err)
@@ -21,4 +34,20 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(user)
 }
 
-// TODO: Add user modification endpoints calling funcs in db-users.go file
+// Structure of the values a user can update in the update user model
+type UpdateUserReqBody struct {
+	Name          *string                `json:"name,omitempty"`
+	Email         *string                `json:"email,omitempty"`
+	ApiClients    []*models.ApiClient    `json:"apiClients,omitempty"`
+	Organisations []*models.Organisation `json:"organisations,omitempty"`
+}
+
+// Handles the update user api calls
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	//userId := getUserId(r)
+
+	resp := map[string]interface{}{
+		"success": true,
+	}
+	apiResponse(resp, w)
+}
